acbe6a8001efc5e983f6027d0a03c974cf25588b
me <me@xdproject.ru>
2025-02-04 19:16:40 +0500

    Done 00_distance.py

--- 00_distance.py
+++ 00_distance.py
@@ -14,7 +14,15 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+for name_1, coords_1 in sites.items():
+    distances[name_1] = {}
+    for name_2, coords_2 in sites.items():
+        if name_1 != name_2:
+            x1 = coords_1[0]
+            y1 = coords_1[1]
+            x2 = coords_2[0]
+            y2 = coords_2[1]
+            distances[name_1][name_2] = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 print(distances)
 
